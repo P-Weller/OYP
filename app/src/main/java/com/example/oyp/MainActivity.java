043dbfdaf984e9476aba8b27715d35006c50aa8c
Pascal <p-weller@gmx.de>
2019-03-19 12:51:48 +0200

    Initial commit -JDBC Connector as library integrated -Navbar Menu working

---
+++ app/src/main/java/com/example/oyp/MainActivity.java
@@ -0,0 +1,73 @@
+package com.example.oyp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener, BottomNavigationView.OnNavigationItemReselectedListener {
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        loadFragment(new TasksFragment());
+
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+        navigation.setSelectedItemId(R.id.navigation);
+
+
+
+
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+
+
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        Fragment fragment = null;
+
+        switch(item.getItemId()) {
+
+            case R.id.navigation_tasks:
+                fragment = new TasksFragment();
+                break;
+            case R.id.navigation_activity:
+                fragment = new ActivityFragment();
+                break;
+            case R.id.navigation_more:
+                fragment = new MoreFragment();
+                break;
+            case R.id.navigation_users:
+                fragment = new UsersFragment();
+                break;
+
+        }
+
+        return loadFragment(fragment);
+    }
+
+
+    public void onNavigationItemReselected(@NonNull MenuItem item) {
+    }
+}
