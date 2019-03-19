ca8073bd11fbc1157cde54dec447efbf7ce2018b
Pascal <p-weller@gmx.de>
2019-03-19 14:31:55 +0200

    Login work

---
+++ app/src/main/java/com/example/oyp/LoginActivity.java
@@ -0,0 +1,9 @@
+package com.example.oyp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+
+public class LoginActivity extends AppCompatActivity {
+
+    EditText ;
+}
