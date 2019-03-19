d21c9c6b579fdcc6dd324019c19168b14049b20e
Paschmi <48715555+Paschmi@users.noreply.github.com>
2019-03-19 14:28:16 +0000

    Merge branch 'master' of C:\Users\ADM_Schmid\Documents\GitHub\OYP with conflicts.

--- app/src/main/java/com/example/oyp/LoginActivity.java
+++ app/src/main/java/com/example/oyp/LoginActivity.java
@@ -1,36 +1,9 @@
 package com.example.oyp;
 
-import android.app.ProgressDialog;
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.WindowManager;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
-    EditText mNameEditText, passwordEditText;
-    Button logIn2Btn;
-
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-        //getSupportActionBar().hide();
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
-
-
-        mNameEditText = (EditText) findViewById(R.id.name);
-        passwordEditText = (EditText) findViewById(R.id.pass);
-        logIn2Btn = (Button) findViewById(R.id.login);
-
-        connectionClass = new ConnectionClass();
-
-        progressDialog=new ProgressDialog(this);
-
-        BackgroundActivity bg = new BackgroundActivity();
-        bg.execute(MName, MPassword);
-
-    }
+    EditText;
 }
