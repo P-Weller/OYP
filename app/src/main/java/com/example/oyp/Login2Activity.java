ae95164bc1a5f1ab5076dbebc005092201a30945
Pascal <p-weller@gmx.de>
2019-03-19 17:02:26 +0200

    LoginActivity extended

---
+++ app/src/main/java/com/example/oyp/Login2Activity.java
@@ -0,0 +1,27 @@
+package com.example.oyp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class Login2Activity extends AppCompatActivity {
+
+
+
+    TextView nametext;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.oyp.R.layout.activity_login2);
+
+        String name=getIntent().getStringExtra("name");
+
+        nametext= (TextView) findViewById(com.example.oyp.R.id.name);
+
+        nametext.setText(name);
+
+
+
+    }
+}
+
--- app/src/main/java/com/example/oyp/LoginActivity.java
+++ app/src/main/java/com/example/oyp/LoginActivity.java
@@ -1,11 +1,18 @@
 package com.example.oyp;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -32,5 +39,105 @@ public class LoginActivity extends AppCompatActivity {
         progressDialog=new ProgressDialog(this);
     }
 
+    private class Dologin extends AsyncTask<String,String,String> {
 
+
+        String namestr=mName.getText().toString();
+        String passstr=mPassword.getText().toString();
+        String z="";
+        boolean isSuccess=false;
+
+        String nm,password;
+
+
+        @Override
+        protected void onPreExecute() {
+
+
+            progressDialog.setMessage("Loading...");
+            progressDialog.show();
+
+
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            if(namestr.trim().equals("")||passstr.trim().equals(""))
+                z = "Please enter all fields...";
+            else
+            {
+                try {
+                    Connection con = connectionClass.CONN();
+                    if (con == null) {
+                        z = "Please check your internet connection";
+                    } else {
+
+                        String query=" select * from master where mName='"+namestr+"' and mPassword = '"+passstr+"'";
+
+
+                        Statement stmt = con.createStatement();
+                        // stmt.executeUpdate(query);
+
+
+                        ResultSet rs=stmt.executeQuery(query);
+
+                        while (rs.next())
+
+                        {
+                            nm= rs.getString(2);
+                            password=rs.getString(3);
+
+
+
+
+                            if(nm.equals(namestr)&&password.equals(passstr))
+                            {
+
+                                isSuccess=true;
+                                z = "Login successful";
+
+                            }
+
+                            else
+
+                                isSuccess=false;
+
+
+
+                        }
+
+
+
+
+
+                    }
+                }
+                catch (Exception ex)
+                {
+                    isSuccess = false;
+                    z = "Exceptions"+ex;
+                }
+            }
+            return z;        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            Toast.makeText(getBaseContext(),""+z,Toast.LENGTH_LONG).show();
+
+
+            if(isSuccess) {
+
+                Intent intent=new Intent(LoginActivity.this,Login2Activity.class);
+
+                intent.putExtra("name",namestr);
+
+                startActivity(intent);
+            }
+
+
+            progressDialog.hide();
+
+        }
+    }
 }
