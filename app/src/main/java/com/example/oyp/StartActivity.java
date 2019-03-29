f23c9cec3bc150a4b1f14aac02b49e89f1c7eb4c
srklink <48717396+srklink@users.noreply.github.com>
2019-03-29 23:41:10 +0200

    StartActivity.java und App Icon

---
+++ app/src/main/java/com/example/oyp/StartActivity.java
@@ -0,0 +1,93 @@
+package com.example.oyp;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class StartActivity extends AppCompatActivity {
+
+
+    Button signupbtn;
+    Button loginbtn;
+
+    /*
+
+    public void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+
+        //Get the view from activity_start.xml
+        setContentView(R.layout.activity_start);
+
+        //Locate the button in activity_start.xml
+        signupbtn = (Button) findViewById(R.id.StartsignupBtn);
+
+        //Capture button clicks
+        signupbtn.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+
+                //Start LoginActivity.class
+                Intent intent = new Intent(StartActivity.this,RegistrationActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    */
+
+    public void onCreate(Bundle savedInstanceState){
+
+        super.onCreate(savedInstanceState);
+
+        //Get the view from activity_start.xml
+        setContentView(R.layout.activity_start);
+
+        //Locate the button in activity_start.xml
+        loginbtn = (Button) findViewById(R.id.StartloginBtn);
+        signupbtn = (Button) findViewById(R.id.StartsignupBtn);
+
+        //Capture button clicks
+        loginbtn.setOnClickListener(new View.OnClickListener(){
+
+            public void onClick(View v){
+
+                //Start LoginActivity.class
+                Intent intent = new Intent(StartActivity.this,LoginActivity.class);
+                startActivity(intent);
+
+            }
+
+
+
+
+            /*
+            signupbtn.setOnClickListener(new View.OnClickListener(){
+
+                public void OnClick(View v2){
+                    //Start blablabla
+                    Intent intent = new Intent(StartActivity.this,LoginActivity.class);
+                    startActivity(intent);
+                }
+            }*/
+
+
+
+
+
+
+        });
+
+    }
+
+
+
+
+
+
+}
