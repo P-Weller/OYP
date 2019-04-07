b005ec3b72a1d2fdb282c9d5353241416f92b3bc
Pascal <p-weller@gmx.de>
2019-04-07 16:55:50 +0200

    Updated Navigation Bar - Create Task & Changed Order

--- app/src/main/java/com/example/oyp/CreateTaskActivity.java
+++ app/src/main/java/com/example/oyp/CreateTaskActivity.java
@@ -1,10 +1,7 @@
 package com.example.oyp;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class CreateTaskActivity extends AppCompatActivity {
@@ -16,10 +13,10 @@ public class CreateTaskActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
 
-        //Get the view from activity_createtask.xml
-        setContentView(R.layout.activity_createtask);
+        //Get the view from fragment_createtask.xml
+        setContentView(R.layout.fragment_createtask);
 
-        //Locate the button in activity_createtask.xml
+        //Locate the button in fragment_createtask.xml
         personET = (EditText) findViewById(R.id.createTaskEditText);
 
 
--- app/src/main/java/com/example/oyp/Fragments/UsersFragment.java
+++ app/src/main/java/com/example/oyp/Fragments/UsersFragment.java
@@ -13,8 +13,7 @@ public class UsersFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_users, container, false);
-
+            return inflater.inflate(R.layout.fragment_users, container, false);
 
     }
 }
