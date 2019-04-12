a59b6072ac5bc8945cc1efbc0efabc10d678c467
Pascal <p-weller@gmx.de>
2019-04-12 13:58:29 +0200

    Connection CreateTaskFragment to ListView to Database implemented and Arrays changed to ArrayLists

--- app/src/main/java/com/example/oyp/Fragments/TasksFragment.java
+++ app/src/main/java/com/example/oyp/Fragments/TasksFragment.java
@@ -1,8 +1,13 @@
 package com.example.oyp.Fragments;
 
+import android.content.Context;
+import android.content.res.Resources;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,27 +15,144 @@ import android.widget.ListView;
 
 import com.example.oyp.OpenTasksAdapter;
 import com.example.oyp.R;
+import com.example.oyp.UnamesAdapter;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class TasksFragment extends Fragment {
+
+    Context thisContext;
+    ArrayList<Integer> tImage = new ArrayList();
+    ArrayList<String> tName = new ArrayList();
+    ListView otasksListView;
 
     public TasksFragment(){
     }
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_tasks, container, false);
+        thisContext = this.getContext();
 
-        int[] tImage = {R.drawable.living, R.drawable.ic_person_add_black_24dp, R.drawable.ic_color_lens_black_32dp, R.drawable.ic_loop_black_32dp,
-        R.drawable.ic_color_lens_black_32dp, R.drawable.ic_attach_money_black_32dp, R.drawable.ic_notifications_black_24dp};
-        String[] tName = {"Clean", "Clean more", "Clean!!", "Ironing", "Clean kitchen", "clean bathroom", "maw the lawn"};
+        otasksListView = view.findViewById(R.id.otasksListView);
 
-        ListView otasksListView = (ListView) view.findViewById(R.id.otasksListView);
-
-        OpenTasksAdapter taskAdapter = new OpenTasksAdapter(this.getContext(),tImage, tName);
-        otasksListView.setAdapter(taskAdapter);
+        GetData retrieveData = new GetData();
+        retrieveData.execute("");
         return view;
 
+    }
 
+    public Connection connectionclass(String user, String password, String database, String server) {
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+        Connection connection = null;
+        String ConnectionURL = null;
+
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            ConnectionURL = "jdbc:mariadb://" + server + "/" + database;
+            connection = DriverManager.getConnection(ConnectionURL, user, password);
+        } catch (SQLException se) {
+            Log.e("error here 1 : ", se.getMessage());
+        } catch (ClassNotFoundException e) {
+            Log.e("error here 2 : ", e.getMessage());
+        } catch (Exception e) {
+            Log.e("error here 3 : ", e.getMessage());
+        }
+        return connection;
+    }
+
+
+    private class GetData extends AsyncTask<String, String, String> {
+        String msg = "";
+
+
+        @Override
+        protected void onPreExecute() {
+
+            super.onPreExecute();
+        }
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            Connection conn = null;
+            Statement stmt = null;
+
+            String ip = "192.168.1.164";
+            String db = "oyp_database";
+            String un = "root";
+            String pass = "pass";
+
+            ArrayList<String> activityIDs = new ArrayList<>();
+
+
+            try {
+                conn = connectionclass(un, pass, db, ip);
+
+                stmt = conn.createStatement();
+                String sql = "SELECT * FROM activity,task WHERE task.ActivityID = activity.ActivityID AND StatusID = 0";
+                ResultSet rs = stmt.executeQuery(sql);
+                int i = 0;
+
+                while (rs.next()) {
+                    String aImageString = rs.getString("AIcon");
+                    String aName = rs.getString("AName");
+
+                    Resources res = getResources();
+                    int aImage = res.getIdentifier(aImageString , "drawable", getActivity().getPackageName());
+
+                    tImage.add(i, aImage);
+                    tName.add(i, aName);
+                    i++;
+
+                }
+
+
+                System.out.println(tImage);
+                System.out.println(tName);
+                msg = "Process complete.";
+                rs.close();
+                stmt.close();
+                conn.close();
+
+
+            } catch (SQLException connError) {
+                msg = "An exception was thrown by JDBC.";
+                connError.printStackTrace();
+            } finally {
+                try {
+                    if (stmt != null) {
+                        stmt.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    if (conn != null) {
+                        conn.close();
+                    }
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+
+
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+
+            OpenTasksAdapter openTasksAdapter = new OpenTasksAdapter(thisContext, tImage, tName);
+            otasksListView.setAdapter(openTasksAdapter);
+        }
     }
 }
 
