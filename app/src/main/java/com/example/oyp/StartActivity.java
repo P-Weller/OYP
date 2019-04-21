a6ac85737c2c9fb24976c4d4cc2da03433d9123c
Paschmi <48715555+Paschmi@users.noreply.github.com>
2019-04-21 16:22:43 +0000

    StartActivity onBackPressed deaktiviert

--- app/src/main/java/com/example/oyp/StartActivity.java
+++ app/src/main/java/com/example/oyp/StartActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -53,6 +54,10 @@ public class StartActivity extends AppCompatActivity {
 
         });
 
+    }
+    @Override
+    public void onBackPressed() {
+        Log.e("######","You can't get out!");
     }
 
 
