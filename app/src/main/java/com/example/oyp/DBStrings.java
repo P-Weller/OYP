b1dc1a505466676199ef9600e2b5fc35a15d1b0d
Paschmi <48715555+Paschmi@users.noreply.github.com>
2019-04-22 12:18:09 +0000

    IP aktualisiert

--- app/src/main/java/com/example/oyp/DBStrings.java
+++ app/src/main/java/com/example/oyp/DBStrings.java
@@ -1,7 +1,7 @@
 package com.example.oyp;
 
 public class DBStrings {
-    public static final String DATABASE_IP = "192.168.1.172";
+    public static final String DATABASE_IP = "192.168.0.103";
     public static final String DATABASE_NAME = "oyp_database";
     public static final String DATABASE_USER = "root";
     public static final String DATABASE_PASSWORD = "pass";
